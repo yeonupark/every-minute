7927022c60906f1737988eba8a04c01764fa601a
yeonupark <idepix@naver.com>
2024-01-06 19:05:48 +0900

    Initial Commit

---
+++ MessengerProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MessengerProject
+//
+//  Created by Yeonu Park on 2024/01/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MessengerProject/MessengerProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerProjectApp.swift
+//  MessengerProject
+//
+//  Created by Yeonu Park on 2024/01/06.
+//
+
+import SwiftUI
+
+@main
+struct MessengerProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
