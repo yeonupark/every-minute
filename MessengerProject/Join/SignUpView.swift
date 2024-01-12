c591a625ad3abd760412dd661f198c823cc5dcea
yeonupark <idepix@naver.com>
2024-01-12 23:58:43 +0900

    SignUpViewModel Combine 작업중

--- MessengerProject/Join/SignUpView.swift
+++ MessengerProject/Join/SignUpView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct SignUpView: View {
     
+    @ObservedObject var viewModel = SignUpViewModel()
+    
     @Binding var isShowingBottomSheet: Bool
     
-    @State var email: String = ""
-    @State var nickname: String = ""
-    @State var phoneNumber: String = ""
-    @State var password: String = ""
-    @State var passwordRepeat: String = ""
+    //@State var email: String = ""
+//    @State var nickname: String = ""
+//    @State var phoneNumber: String = ""
+//    @State var password: String = ""
+//    @State var passwordRepeat: String = ""
     
     var body: some View {
         
@@ -24,19 +26,21 @@ struct SignUpView: View {
                 ColorSet.Background.primary
                 VStack(alignment: .center, spacing: 24) {
                     
-                    EmailField(label: "이메일", placeholder: "이메일을 입력하세요", input: email)
+                    EmailField(label: "이메일", placeholder: "이메일을 입력하세요", input: $viewModel.email.value)
                         .padding(.top, 24)
-                    InputField(label: "닉네임", placeholder: "닉네임을 입력하세요", input: nickname)
-                    InputField(label: "연락처", placeholder: "전화번호를 입력하세요", input: nickname)
-                    InputField(label: "비밀번호", placeholder: "비밀번호를 입력하세요", input: nickname)
-                    InputField(label: "비밀번호 확인", placeholder: "비밀번호를 한 번 더 입력하세요", input: nickname)
+                    InputField(label: "닉네임", placeholder: "닉네임을 입력하세요", input: $viewModel.nickname.value)
+                    InputField(label: "연락처", placeholder: "전화번호를 입력하세요", input: $viewModel.phoneNumber.value)
+                    InputField(label: "비밀번호", placeholder: "비밀번호를 입력하세요", input: $viewModel.password.value)
+                    InputField(label: "비밀번호 확인", placeholder: "비밀번호를 한 번 더 입력하세요", input: $viewModel.passwordRepeat.value)
                     
                     Spacer()
                     
                     Button(action: {
+                        print(viewModel.email.value)
                         print("가입하기")
                     }, label: {
-                        Image(.joinButton)
+                        //Image(.joinButton)
+                        viewModel.signUpButtonImage.value
                             .resizable()
                             .frame(width: 345, height: 44)
                     })
@@ -45,21 +49,16 @@ struct SignUpView: View {
             .navigationTitle("회원가입")
             .navigationBarTitleDisplayMode(.inline)
             .navigationBarItems(leading: closeBarButton(isShowingBottomSheet: $isShowingBottomSheet))
-            
         }
     }
 }
-
-//#Preview {
-//    SignUpView()
-//}
 
 struct InputField: View {
     
     let label: String
     let placeholder: String
     
-    @State var input: String
+    @Binding var input: String
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -84,7 +83,7 @@ struct EmailField: View {
     let label: String
     let placeholder: String
     
-    @State var input: String
+    @Binding var input: String
     
     var body: some View {
         VStack(alignment: .leading) {
--- MessengerProject/Join/SignUpViewModel.swift
+++ MessengerProject/Join/SignUpViewModel.swift
@@ -5,8 +5,51 @@
 //  Created by Yeonu Park on 2024/01/11.
 //
 
-import Foundation
+import Combine
+import SwiftUI
 
-class SignUpViewModel {
+class SignUpViewModel: ObservableObject {
     
+    var email = CurrentValueSubject<String, Error>("")
+    var nickname = CurrentValueSubject<String, Error>("")
+    var phoneNumber = CurrentValueSubject<String, Error>("")
+    var password = CurrentValueSubject<String, Error>("")
+    var passwordRepeat = CurrentValueSubject<String, Error>("")
+    var deviceToken = CurrentValueSubject<String, Error>("")
+    
+    var signUpButtonImage = CurrentValueSubject<Image, Error>(Image(.joinButton))
+    
+    init() {
+        fieldCheck()
+    }
+    
+    func fieldCheck() {
+        
+        var cancellables: Set<AnyCancellable> = []
+        
+        var validation: AnyPublisher<Bool, Error> {
+                return Publishers.CombineLatest4(email, nickname, password, passwordRepeat)
+                    .map { email, nick, pw, pw2 in
+                        if email.isEmpty || nick.isEmpty || (pw != pw2) {
+                            return false
+                        }
+                        return true
+                    }
+                    .eraseToAnyPublisher()
+            }
+
+        validation
+                .map { isValid in
+                    isValid ? Image(.joinButtonEnabled) : Image(.joinButton)
+                }
+                .receive(on: DispatchQueue.main)
+                .sink(receiveCompletion: { completion in
+                        // 에러 처리를 추가할 수 있음
+                        // completion에는 성공 또는 에러가 포함됨
+                    }, receiveValue: { [weak self] image in
+                        self?.signUpButtonImage.send(image)
+                    })
+                .store(in: &cancellables)
+        
+    }
 }
